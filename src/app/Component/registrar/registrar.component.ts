2a32dca85fbb5a0defb60062bb8bcf906577a7dc
Eddy-Boo <68482691+Eddy-Boo@users.noreply.github.com>
2020-07-24 14:37:24 -0500

    registrar-interface

--- src/app/Component/registrar/registrar.component.ts
+++ src/app/Component/registrar/registrar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as AWS from 'aws-sdk';
+import { Buffer } from 'buffer';
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.component.css']
 })
 export class RegistrarComponent implements OnInit {
+  @ViewChild("video") video: ElementRef;
+  @ViewChild("canvas") canvas: ElementRef;
+
+  foto: any;
+  detector: any;
+  image: any;
+  maxConfidence: any;
+  nombreEmocion: any;
+  cargando = null;
+  bucket        = 'bucket' // the bucketname without s3://
+  foto_origen  = 'source.jpg'
+  client:any;
+  response : any
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  public ngAfterViewInit() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(screenStream => {
+        this.video.nativeElement.srcObject = screenStream;
+        this.video.nativeElement.play();
+      });
+    }
+  }
 
+  public capturar() {
+    this.cargando = true;
+    this.nombreEmocion = null;
+    var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 600, 440);
+    this.foto = this.canvas.nativeElement.toDataURL("image/png");
+    this.foto = this.foto.split(",")[1];
+    this.image = {
+      Image: {
+        Bytes: new Buffer(this.foto, 'base64')
+      },
+      Attributes: ['ALL']
+    }
+    
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Component/home/home.component';
 import { IniciarSesionComponent } from './Component/iniciar-sesion/iniciar-sesion.component';
+import { RegistrarComponent } from './Component/registrar/registrar.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,10 @@ const routes: Routes = [
     path: 'Iniciar-sesion',
     component: IniciarSesionComponent
   },
+{
+  path: 'registrar',
+  component: RegistrarComponent
+},
   
 ];
 
