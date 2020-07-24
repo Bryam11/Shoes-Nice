b703e5047d231697a6ebc5e0e8e12977b045d850
Eddy-Boo <68482691+Eddy-Boo@users.noreply.github.com>
2020-07-23 19:10:15 -0500

    iniciar-sesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Component/home/home.component';
+import { IniciarSesionComponent } from './Component/iniciar-sesion/iniciar-sesion.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
